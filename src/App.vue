51e5032a179081a744922e0e9469f208172d3a03
Cedric <cedric.tellier@yahoo.fr>
2020-06-23 22:23:59 +0200

    add vuetify dependency

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h5>Cédric Tellier website</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse at vero saepe possimus maxime culpa placeat hic voluptate, officiis adipisci quas, vel suscipit doloribus similique nesciunt, quae voluptatibus maiores sunt veritatis corporis optio id praesentium iusto fugiat. Autem saepe enim aspernatur iusto reiciendis porro ea suscipit, ipsam dolor harum neque!</p>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-</script>
+import HelloWorld from './components/HelloWorld';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
