a05d6e3b7734ecfae0e0398ea3c4884d200905a0
Cedric <cedric.tellier@yahoo.fr>
2020-06-26 22:06:32 +0200

    remove useless helloworld code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a Home page</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  // name: 'Home',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
