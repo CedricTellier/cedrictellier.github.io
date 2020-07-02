2245a651c144c7fbf6c94da27837919102e4da29
Cedric <cedric.tellier@yahoo.fr>
2020-07-02 20:37:55 +0200

    button routes tests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <h1>This is a Home page</h1>
+    <div>
+      <router-link to="/about" v-slot="{ href, route, navigate}">
+        <v-btn  :href="href" @click="navigate" class="primary">Go to About</v-btn >
+      </router-link>
+       <router-link to="/projects" v-slot="{ href, route, navigate}">
+        <v-btn  :href="href" @click="navigate" class="success">Go to Projects</v-btn >
+      </router-link>
+       <router-link to="/contact" v-slot="{ href, route, navigate}">
+        <v-btn :href="href" @click="navigate" class="error">Go to COntact</v-btn >
+      </router-link>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  // name: 'Home',
-  // components: {
-  //   HelloWorld
-  // }
-}
+  data: () => ({})
+};
 </script>
