3b92ab266a881dfc9f17f20a4f32668c826f8bfb
Cedric Tellier <cedric.tellier25@gmail.com>
2020-07-10 12:05:22 +0200

    test download Process

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,15 +2,23 @@
   <div class="contact">
     <h1>This is a Contact page</h1>
     <ReturnButton></ReturnButton>
+      <a href="test.png" download="logo">
+        <img src="test.png" alt="test" />
+      </a>
   </div>
 </template>
 
 <script>
-  import ReturnButton from '@/components/ReturnButton.vue'
-    export default {
-    name: 'Contact',
-    components: {
-      ReturnButton
+import ReturnButton from "@/components/ReturnButton.vue";
+export default {
+  name: "Contact",
+  components: {
+    ReturnButton
+  },
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
     }
   }
+};
 </script>
