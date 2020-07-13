060e3bd17a52886396c32c7a6d6a513949ba819b
Cedric Tellier <cedric.tellier25@gmail.com>
2020-07-13 15:46:24 +0200

    new token

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -160,7 +160,7 @@ export default {
       this.submitted = true;
       var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
       Email.send({
-        SecureToken : "e08927f5-cf11-4d67-890a-f719f24b136d",
+        SecureToken : "d87158d1-a702-4946-9586-525b7de3729b",
         To : 'cedric.tellier@yahoo.fr',
         From : "cedric.tellier25@gmail.com",
         Subject : "This is the subject",
